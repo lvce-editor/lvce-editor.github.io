82b7ba836b705519b4e59e66ce546b4fdfeec7bb
Le Vivilet <levivilet@pm.me>
2026-01-03 12:52:06 +0100

    feature: use external icon (#29)

--- packages/website/app.vue
+++ packages/website/app.vue
@@ -117,7 +117,7 @@ body {
   width: 100%;
   height: 100%;
   opacity: var(--grain-opacity);
-  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+  background-image: url("/grain-overlay.svg");
   pointer-events: none;
   z-index: 2;
   mix-blend-mode: overlay;
