3a61715f687e76c87c472f071c1e7d4956fa8ee3
Le Vivilet <levivilet@pm.me>
2026-01-03 09:55:15 +0100

    feature: improve build script

--- packages/build/src/build.ts
+++ packages/build/src/build.ts
@@ -1,12 +1,21 @@
 import { execa } from "execa";
 import { join } from "node:path";
 import { root } from "./root.ts";
+import { cp } from "node:fs/promises";
 
 const main = async () => {
   await execa(`npm`, [`run`, "build"], {
     stdio: "inherit",
     cwd: join(root, "packages", "website"),
   });
+  await cp(
+    join(root, "packages", "website", ".output", "public"),
+    join(root, "packages", "build", ".tmp", "dist"),
+    {
+      recursive: true,
+      force: true,
+    }
+  );
 };
 
 main();
