fbd1ced1f0593df1253348fcdc6f304a607d056c
Le Vivilet <levivilet@pm.me>
2026-01-03 11:39:54 +0100

    feature: improve build

--- packages/build/src/build.ts
+++ packages/build/src/build.ts
@@ -1,21 +1,20 @@
 import { execa } from "execa";
-import { join } from "node:path";
+import { dirname, join } from "node:path";
 import { root } from "./root.ts";
-import { cp } from "node:fs/promises";
+import { cp, mkdir, rm } from "node:fs/promises";
 
 const main = async () => {
   await execa(`npm`, [`run`, "build"], {
     stdio: "inherit",
     cwd: join(root, "packages", "website"),
   });
-  await cp(
-    join(root, "packages", "website", ".output", "public"),
-    join(root, "packages", "build", ".tmp", "dist"),
-    {
-      recursive: true,
-      force: true,
-    },
-  );
+  const dist = join(root, "packages", "build", ".tmp", "dist");
+  await rm(dist, { recursive: true });
+  await mkdir(dirname(dist), { recursive: true });
+  await cp(join(root, "packages", "website", ".output", "public"), dist, {
+    recursive: true,
+    force: true,
+  });
 };
 
 main();
