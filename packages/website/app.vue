76101a583b0d598b217707f96c078c2d8a350898
Le Vivilet <levivilet@pm.me>
2026-01-03 14:00:41 +0100

    feature: simplify font config (#45)

--- packages/website/app.vue
+++ packages/website/app.vue
@@ -16,15 +16,6 @@ onMounted(() => {
 </script>
 
 <style>
-@import "@fontsource/inter/300.css";
-@import "@fontsource/inter/400.css";
-@import "@fontsource/inter/500.css";
-@import "@fontsource/inter/600.css";
-@import "@fontsource/inter/700.css";
-@import "@fontsource/jetbrains-mono/400.css";
-@import "@fontsource/jetbrains-mono/500.css";
-@import "@fontsource/jetbrains-mono/600.css";
-
 :root {
   /* Light theme (default) */
   --bg-primary: #ffffff;
