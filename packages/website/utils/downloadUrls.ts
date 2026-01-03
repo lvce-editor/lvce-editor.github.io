9a2973bb6c64de3034ef9c5d81d086561ae96c78
Le Vivilet <levivilet@pm.me>
2026-01-03 10:08:27 +0100

    fix

--- packages/website/utils/downloadUrls.ts
+++ packages/website/utils/downloadUrls.ts
@@ -10,7 +10,7 @@ const DEFAULT_RELEASE_URL_BASE =
 
 export function getDownloadUrl(
   platform: Platform,
-  config: DownloadConfig
+  config: DownloadConfig,
 ): string {
   const { version } = config;
   const releaseUrlBase = config.releaseUrlBase || DEFAULT_RELEASE_URL_BASE;
@@ -27,7 +27,7 @@ export function getDownloadUrl(
 }
 
 export function getAllDownloadUrls(
-  config: DownloadConfig
+  config: DownloadConfig,
 ): Record<Platform, string> {
   return {
     linux: getDownloadUrl("linux", config),
